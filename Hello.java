edcf0bd41de56c97380ae2dbaf920e7892f24da5
davism12 <59926282+davism12@users.noreply.github.com>
2020-01-15 10:27:26 -0500

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,20 @@
+/***********************************************
+* Hello Java
+************************************************
+* Mikayla Davis
+* January 15, 2019
+* 255 and 003
+************************************************/
+public class Hello {
+	public static void main (String[] args) {
+	
+		System.out.println("<Mikayla>");
+		System.out.println("<Hello>");
+		System.out.println("<Java Project Name>");
+		System.out.println("<Project Description>");
+		System.out.println("<January 15th>");
+		System.out.println("<255 and 003>");
+		System.out.println();
+		}
+		
+	}
